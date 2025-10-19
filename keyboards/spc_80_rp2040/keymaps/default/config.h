eeb73ca152a2cb46388d5e17fecca86730b17fbd
prifak <aaeffff@gmail.com>
2025-10-19 02:59:15 +0200

    add keyboard on rp2040 base (spc 530)

---
+++ keyboards/spc_80_rp2040/keymaps/default/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define LED1_PIN GP23
+#define LED2_PIN GP24
+#define LED3_PIN GP25
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
---
+++ keyboards/spc_80_rp2040/keymaps/default/keymap.c
@@ -0,0 +1,191 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "quantum.h"
+
+enum via_layers {
+    BASE,
+    FN1,
+    FN2,
+    FN3,
+};
+// Определяем свои кейкоды
+enum custom_keycodes {
+    LED1_TOG = SAFE_RANGE, // Начинаем с SAFE_RANGE
+    LED2_TOG,
+    LED3_TOG,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+     * │Esc│   │F1 │F2 │F3 │F4 │ │F5 │F6 │F7 │F8 │ │F9 │F10│F11│F12│ │PSc│Scr│Pse│
+     * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│ │Ins│Hom│PgU│
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │ │Del│End│PgD│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+     * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐
+     * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │    Shift │     │ ↑ │
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+     * │Ctrl│GUI │Alt │                        │ Alt│ GUI│Menu│Ctrl│ │ ← │ ↓ │ → │
+     * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+     */
+    [BASE] = LAYOUT_tkl_ansi(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+        MO(FN1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(FN2), MO(FN3),  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [FN1] = LAYOUT_tkl_ansi(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, _______, _______,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [FN2] = LAYOUT_tkl_ansi(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,
+        BL_TOGG, BL_DOWN, BL_UP,                            KC_SPC,                             KC_RALT, _______, _______,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [FN3] = LAYOUT_tkl_ansi(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, _______, _______,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+    )
+};
+
+
+
+
+
+
+//static bool led1_state = false;
+//static bool led2_state = false;
+//static bool led3_state = false;
+
+void keyboard_post_init_user(void) {
+    setPinOutput(GP23);
+    setPinOutput(GP24);
+    setPinOutput(GP25);
+
+    writePinLow(GP23);
+    writePinLow(GP24);
+    writePinLow(GP25);
+
+    setPinOutput(GP26);
+    setPinOutput(GP27);
+
+    writePinHigh(GP26);
+    writePinHigh(GP27);
+}
+/*
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case LED1_TOG:
+                led1_state = !led1_state;
+                if (led1_state) {
+                    writePinHigh(GP23);
+                } else {
+                    writePinLow(GP23);
+                }
+                return false;
+
+            case LED2_TOG:
+                led2_state = !led2_state;
+                if (led2_state) {
+                    writePinHigh(GP24);
+                } else {
+                    writePinLow(GP24);
+                }
+                return false;
+
+            case LED3_TOG:
+                led3_state = !led3_state;
+                if (led3_state) {
+                    writePinHigh(GP25);
+                } else {
+                    writePinLow(GP25);
+                }
+                return false;
+        }
+    }
+    return true;
+}
+*/
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case BL_TOGG: // Управляем LED1
+                // Просто инверсия состояния без хранения
+                if (readPin(GP25)) {
+                    writePinLow(GP25);
+                } else {
+                    writePinHigh(GP25);
+                }
+                return false;
+
+            case BL_UP: // Управляем LED2
+                if (readPin(GP24)) {
+                    writePinLow(GP24);
+                } else {
+                    writePinHigh(GP24);
+                }
+                return false;
+
+            case BL_DOWN: // Управляем LED3
+                if (readPin(GP23)) {
+                    writePinLow(GP23);
+                } else {
+                    writePinHigh(GP23);
+                }
+                return false;
+        }
+    }
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Сначала всё погасим
+    writePinHigh(GP26);
+    writePinHigh(GP27);
+
+    switch (get_highest_layer(state)) {
+        case 1: // FN1
+            writePinLow(GP27);
+            break;
+        case 2: // FN2
+            writePinLow(GP26);
+            break;
+        case 3: // FN3
+            writePinLow(GP26);
+            writePinLow(GP27);
+            break;
+        default: // Base layer
+            // оба погашены
+            break;
+    }
+
+    return state;
+}
+
+
